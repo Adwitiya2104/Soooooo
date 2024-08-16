3aeec378674975a9293f02cc2a5f142551130032
Adwitiya Paul <adwitiya.paul2104@gmail.com>
2024-08-17 00:05:48 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "../../baby/src/components/HomePage";
+import MazePage from "../../baby/src/components/MazePage";
+import MainPage from "../../baby/src/components/MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/maze/:pageId" element={<MazePage />} />
+        <Route path="/main" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
 }
 
